bd5150814aa0a44a212edd50d11f05959deb7866
salmanqureshi-olr <salman.qureshi@olrretail.com>
2026-04-16 21:59:39 +0100

    smaller logo

--- issuer-web-vs/issuer-web/src/routes.ts
+++ issuer-web-vs/issuer-web/src/routes.ts
@@ -308,7 +308,7 @@ function renderPage(serviceName: string, schema: SchemaInfo): string {
 </head>
 <body>
   <header>
-    <img class="logo" src="https://gist.github.com/salmanqureshi-olr/88659be683fde320ef7d31ed44e528e0" alt="UCD logo" />
+    <img class="logo" src="https://gist.github.com/user-attachments/assets/d98e2aa4-1af9-420f-94a2-ae387fc3b6db" alt="UCD logo" />
     <h1>${escapeHtml(serviceName)}</h1>
     <p>Receive Credentials for UCD Student Login</p>
   </header>
